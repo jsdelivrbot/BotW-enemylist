d73ca8209d504635704f5b304ed3aa1cb5d1e66e
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-19 16:10:07 -0700

    Rendered test sample

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import FoeList from '../containers/FoeList';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-       Hello From App Component
+       <FoeList />
       </div>
     );
   }

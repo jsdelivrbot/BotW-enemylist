a56ad571ebd8f3f6b1b2bb15fae088b7a0c7f150
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-19 17:31:21 -0700

    Generated list of foes

--- src/containers/FoeList.js
+++ src/containers/FoeList.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectFoe } from '../actions/index';
+import { bindActionCreators } from 'redux'
 
-export default class FoeList extends Component {
+class FoeList extends Component {
+  foeList() {
+    return this.props.foes.map(foe => {
+      return (
+        <li
+          key={foe.name}
+          onClick={() => this.props.selectFoe(foe)}
+          className="list-group-item">{foe.name}</li>
+      );
+    });
+  }
+
   render() {
     return (
-      <ul>
-        Hi
+      <ul className="list-group col-sm-4">
+        {this.foeList()}
       </ul>
     );
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    //foes came from /src/reducers/index.js
+    foes: state.foes
+  }
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectFoe: selectFoe }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FoeList);

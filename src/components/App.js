c267e4c3f9b27495b13b0362df3ce8128ea9c905
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-19 18:03:42 -0700

    Added FoeDetail to main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import FoeList from '../containers/FoeList';
+import FoeDetail from '../containers/FoeDetail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
        <FoeList />
+       <FoeDetail />
       </div>
     );
   }
